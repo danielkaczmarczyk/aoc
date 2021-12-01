e940a20af916fb4591d0be461c67088a50604229
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2021-12-01 12:32:45 -0800

    refactor + format

--- 01/solutions_01.py
+++ 01/solutions_01.py
@@ -3,28 +3,29 @@
 def parse_depths(depth_string):
     return int(depth_string.strip())
 
+
 with open('./01/input') as file:
     depths = list(map(parse_depths, file.readlines()))
 
+
 def solve_part_1():
-    increases = 0 
-    for i in range(len(depths)):
-        if i == len(depths) - 1:
-            break
+    increases = 0
+    for i in range(len(depths) - 1):
         if depths[i] < depths[i + 1]:
             increases += 1
-    print(increases)
+    return increases
+
 
 def get_window_sum(i, depths):
     return depths[i] + depths[i+1] + depths[i+2]
 
+
 def solve_part_2():
     increases = 0
-    for i in range(len(depths)):
-        if i == len(depths) - 3:
-            break
+    for i in range(len(depths) - 3):
         if get_window_sum(i, depths) < get_window_sum(i + 1, depths):
             increases += 1
-    print(increases)
+    return increases
 
-solve_part_2()
+
+print(solve_part_1(), solve_part_2())
