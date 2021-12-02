39b2fe73e681eabe1f6f4b9ced052a8c55c9049b
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2021-12-02 09:51:26 -0800

    solved pt2

---
+++ 02/solutions_02.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+def parse_directions(command):
+    direction, magnitude = command.split()
+    return direction, int(magnitude)
+
+
+with open('./02/input') as file:
+    directions = list(map(parse_directions, file.readlines()))
+
+
+def solve_part_1():
+    depth = 0
+    horizontal_position = 0
+    for direction in directions:
+        if direction[0] == 'forward':
+            horizontal_position += direction[1]
+        elif direction[0] == 'up':
+            depth -= direction[1]
+        elif direction[0] == 'down':
+            depth += direction[1]
+    return depth * horizontal_position
+
+
+def solve_part_2():
+    aim = 0
+    depth = 0
+    horizontal_position = 0
+    for direction in directions:
+        if direction[0] == 'forward':
+            horizontal_position += direction[1]
+            depth += aim * direction[1]
+        elif direction[0] == 'up':
+            aim -= direction[1]
+        elif direction[0] == 'down':
+            aim += direction[1]
+    return depth * horizontal_position
+
+
+print(solve_part_1(), solve_part_2())
